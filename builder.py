6f05da5407f869fbb2c6d5a0ce25e3c6be2fef18
Tommy Chu <chutommy@fit.cvut.cz>
2023-03-31 16:52:07 +0200

    Minimize file structure

--- builder.py
+++ builder.py
@@ -1,7 +1,58 @@
+from abc import ABC
+from functools import wraps
+
 import numpy as np
 from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit
 
-from oracle import ClassicalOracle, QuantumOracle
+
+def count_incrementer(method):
+    """Increments the Oracles' query counter by one."""
+
+    @wraps(method)
+    def _impl(self, *method_args, **method_kwargs):
+        self.query_count += 1
+        return method(self, *method_args, **method_kwargs)
+
+    return _impl
+
+
+class Oracle(ABC):
+    """A base oracle with unexposed function."""
+
+    def __init__(self, secret: np.array):
+        self.complexity = len(secret)
+        self.secret = secret
+        self.query_count = 0
+
+
+class ClassicalOracle(Oracle):
+    """Represents an implementation of a classical oracle for the Bernstein-Vazirani problem."""
+
+    @count_incrementer
+    def query(self, inp: np.array) -> bool:
+        """Apply the classical BV function."""
+        product = np.dot(self.secret, inp)
+        out_bit = product % 2
+        return out_bit
+
+
+class QuantumOracle(Oracle):
+    """Represents an implementation of a quantum oracle for the Bernstein-Vazirani problem. The oracle is not queried
+    like classical oracles, instead it is applied on a given quantum circuit with exactly specified operational
+    quantum registers (input-output quantum bits)."""
+
+    @count_incrementer
+    def apply_circuit(self, circuit: QuantumCircuit, in_qreg: QuantumRegister, out_qreg: QuantumRegister):
+        """Constructs an oracle within the given quantum circuit on top of the input/output qubits."""
+        # ensure correct size of quantum registers
+        if in_qreg.size != self.complexity:
+            raise ValueError("Invalid input register size.")
+        if out_qreg.size != 1:
+            raise ValueError("Invalid output register size.")
+
+        for i in range(self.complexity):
+            if self.secret[self.complexity - i - 1]:
+                circuit.cx(in_qreg[i], out_qreg[0])
 
 
 class ClassicalSolver:
@@ -12,12 +63,11 @@ class ClassicalSolver:
 
     def solve(self) -> np.array:
         """Queries over all positional bits to determine the secret value of all indices."""
-        n = self.oracle.complexity
-        solution = np.empty(n, dtype=np.byte)
+        solution = np.empty(self.oracle.complexity, dtype=np.byte)
 
-        for i in range(n):
+        for i in range(self.oracle.complexity):
             # create a query with a single on-bit at exposing input index
-            input_val = np.zeros(n, dtype=np.byte)
+            input_val = np.zeros(self.oracle.complexity, dtype=np.byte)
             input_val[i] = 1
 
             answer = self.oracle.query(input_val)
@@ -35,9 +85,9 @@ class BVAlgBuilder:
     def __init__(self, oracle: QuantumOracle):
         self.oracle = oracle
 
-        self.qreg = None  # quantum register for querying
-        self.auxreg = None  # auxiliary register for output qubit
-        self.creg = None  # classical register for measurement
+        self.qreg = None  # quantum BV query register
+        self.auxreg = None  # auxiliary output qubit register
+        self.creg = None  # classical measurement register
 
         self.circuit = None
 
@@ -52,7 +102,8 @@ class BVAlgBuilder:
         for qubit in self.qreg:
             self.circuit.h(qubit)
 
-        # prepare auxiliary qubit to eigenvector of the CX gate
+        # prepare auxiliary qubit to eigenvector of eigenvalue -1 of the CX gate
+        # https://en.wikipedia.org/wiki/Controlled_NOT_gate
         self.circuit.x(self.auxreg)
         self.circuit.h(self.auxreg)
         # self.circuit.h(self.auxreg)
@@ -65,6 +116,7 @@ class BVAlgBuilder:
         self.circuit.measure(self.qreg, self.creg)
 
     def create_circuit(self):
+        """Builds a quantum implementation of the Bernstein-Vazirani's algorithm with the preset secret."""
         self.__allocate_registers()
         self.__prepare_initial_state()
 
