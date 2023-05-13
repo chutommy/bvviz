0b5803f805aee5ef6dd7438a1b5ea484f573dec4
Tommy Chu <chutommy@fit.cvut.cz>
2023-05-13 13:19:08 +0200

    Update page.py

--- src/bvviz/page.py
+++ src/bvviz/page.py
@@ -263,6 +263,7 @@ def render_footer(des: Descriptor, ctx: Dict[str, Any]) -> None:
     with cols[0]:
         st.subheader(':violet[Disclaimer]:', anchor=False)
         st.write(des['text_disclaimer']())
+        st.markdown('Explore [source code](https://github.com/chutommy/bvviz).')
         st.write('Explore <a href="https://github.com/chutommy/bvviz" target="_blank">source code</a>.')
 
     with cols[1]:
